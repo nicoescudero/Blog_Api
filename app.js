9cae42a688f78afb111edb3ffb4b3e2dbbdc4252
nicoescudero <nicolas.escudero00@gmail.com>
2022-08-25 17:19:11 -0300

    fix cors

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({ origin: `${process.env.LIVE_URL}` }));
-app.use('/', indexRouter);
+app.use('/', cors({ origin: `${process.env.LIVE_URL}` }), indexRouter);
 
 module.exports = app;
